fcf9e79fc545d283b2d46f74c1490f80cc57730c
Stickman008 <arm.supakorn@gmail.com>
2023-01-27 19:33:14 +0700

    Add signup with hashed password

--- backend/server.js
+++ backend/server.js
@@ -14,13 +14,20 @@ app.use((req, res, next) => {
 })
 
 // routes
+app.use(express.json())
 app.get('/', (req, res) => {
-    res.json({mess: 'main'})
+    res.json({ mess: 'main' })
 })
 app.use('/api/user', userRoutes)
 app.use('/api/users', userRoutes)
 
-// listen for requests
-app.listen(process.env.PORT || 4000, () => {
-    console.log('listening on port 4000');
-})
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        // listen for requests
+        app.listen(process.env.PORT, () => {
+            console.log('connected to db & listening on port', process.env.PORT)
+        })
+    })
+    .catch((error) => {
+        console.log(error)
+    })
