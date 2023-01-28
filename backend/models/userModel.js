e849b8ed404a94dd905814a6361d8b69ac5fe91e
Stickman008 <arm.supakorn@gmail.com>
2023-01-28 10:36:44 +0700

    Add middleware

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -45,6 +45,7 @@ userSchema.statics.signup = async function (email, password) {
 // static login method
 userSchema.statics.login = async function (email, password) {
 
+    // validation
     if (!email || !password) {
         throw Error('All fields must be filled')
     }
