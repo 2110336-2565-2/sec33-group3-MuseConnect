baa38bafab13444ce0f68fe639657f146e968e36
ohmkunn <ohmkunn@hotmail.com>
2023-02-02 08:26:37 +0700

    conflit test

---
+++ frontend/nextjs-blog/pages/login.js
@@ -0,0 +1,25 @@
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+import 'bootstrap/dist/css/bootstrap.min.css' ;
+
+export default function Login() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+
+      <main>
+       
+      </main>
+
+      <footer>
+    
+      </footer>
+
+      <style jsx>{`
+        
+      `}</style>
+    </div>
+  )
+}
