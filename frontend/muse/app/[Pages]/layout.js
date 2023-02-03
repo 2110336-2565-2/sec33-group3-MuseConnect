e93640726dd9c474996a8aafcf459a700c260556
ohm <ohmkunn@hotmail.com>
2023-02-03 15:47:59 +0700

    move to next 13

--- frontend/muse/app/[Pages]/layout.js
+++ frontend/muse/app/[Pages]/layout.js
@@ -5,6 +5,11 @@ import Link from 'next/link';
 
 export default function RootLayout({ children }) {
   return (
-    <body>{children}</body>
+    <body>
+        <div><SideBar/></div>
+        
+        {children}
+    </body>
+
   )
 }
--- frontend/muse/app/page.js
+++ frontend/muse/app/page.js
@@ -2,15 +2,14 @@ import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from './page.module.css'
 import Link from 'next/link'
+import SideBar from '@/ui/SideBar'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-        <h1>
-          <Link href="/profile">
-            Profile
-          </Link>
-        </h1>
+        <body>
+          <SideBar/>
+        </body>
   )
 }
