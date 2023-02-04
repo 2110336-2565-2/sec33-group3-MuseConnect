ec42c8fa38ad5d42e23d036024a4edbe7fdf7019
rata <rataprim30026@gmail.com>
2023-02-04 10:39:03 +0700

    implement sign up page

--- frontend/muse/app/page.js
+++ frontend/muse/app/page.js
@@ -2,14 +2,16 @@ import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from './page.module.css'
 import Link from 'next/link'
-import SideBar from '@/ui/SideBar'
+//import SideBar from './ui/SideBar'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
         <body>
-          <SideBar/>
+          {/* <SideBar/> */}
+          {/* There is a conflict with SideBar from './ui/SideBar', I will try to solve this later */}
+          <Link href="/Signup">Sign up page</Link>
         </body>
   )
 }
--- frontend/muse/ui/loginform.js
+++ frontend/muse/ui/loginform.js
@@ -1,22 +1,84 @@
+'use client'
 import { Formik, Field, Form } from 'formik';
-
+import Link from 'next/link'
+//-----------------
+import { createContext } from 'react'
+const Context = createContext()
+//-----------------
+// If you find an error from this file, it's probably that you haven't installed
+// formik. Please use 'npm install formik --save' command to install.
 export default function LoginForm() {
     return (
+        <div>
+        <h1>Muse Connect</h1>
+        <h3>Find your new music experiences here.</h3>
         <Formik
-            initialValues={{
-                username: '',
-                password: '',
-            }}
+        initialValues={{email: '', password:'', firstname:'', 
+                        lastname:'', type:''}}
 
-            onSubmit={() => {
-
-            }}
+        onSubmit={(values, actions) => {
+            setTimeout(() => {
+            alert(JSON.stringify(values, null, 2));
+            actions.setSubmitting(false);
+            }, 1000);
+        }}
         >
-            <Form>
-                <Field id="username" name="username" placeholder="Username" />
-                <Field type="password" id="password" name="password" placeholder="Password" />
-                <button type="submit">Login</button>
-            </Form>
+        {props => (
+            <form onSubmit={props.handleSubmit}>
+                <div>
+                    <p>Email</p>
+                    <input
+                        type="text"
+                        onChange={props.handleChange}
+                        onBlur={props.handleBlur}
+                        value={props.values.email}
+                        name="email"
+                    />
+                </div>
+
+                <div>
+                    <p>Create a password</p>
+                    <input
+                        type="text"
+                        onChange={props.handleChange}
+                        onBlur={props.handleBlur}
+                        value={props.values.password}
+                        name="password"
+                    />
+                </div>
+            
+                <div>
+                    <p>Name</p>
+                    <input
+                        type="text"
+                        onChange={props.handleChange}
+                        onBlur={props.handleBlur}
+                        value={props.values.firstname}
+                        name="firstname"
+                    />
+                </div>
+
+                <div>
+                    <p>Last name</p>
+                    <input
+                        type="text"
+                        onChange={props.handleChange}
+                        onBlur={props.handleBlur}
+                        value={props.values.lastname}
+                        name="lastname"
+                    />
+                </div>
+
+            {/* {props.errors.email && <div id="feedback">{props.errors.email}</div>} */}
+            <div>
+                <button type="submit">Sign up</button>
+            </div>
+            <div>
+                <p>Have an account? <Link href="/Signup">Sign in</Link></p>
+            </div>
+            </form>
+        )}
         </Formik>
+    </div>
     )
 }
