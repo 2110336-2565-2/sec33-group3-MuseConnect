58b510b334a9d430cc7ace7e43a633c9f782fffc
ohm <ohmkunn@hotmail.com>
2023-02-07 09:30:13 +0700

    home implement

--- frontend/muse/app/Home/Serch/page.js
+++ frontend/muse/app/Home/Serch/page.js
@@ -7,7 +7,7 @@ import './style.css'
 export default function page() {
   return (
     <div>
-
+      anotherpage
     </div>
   )
 }
--- frontend/muse/app/Home/layout.js
+++ frontend/muse/app/Home/layout.js
@@ -3,11 +3,12 @@ import Button from 'react-bootstrap/Button';
 import Offcanvas from 'react-bootstrap/Offcanvas';
 import Link from 'next/link';
 import SideBar from '../../ui/SideBar'
+import NavBar from '../../ui/NavBar';
 
 export default function RootLayout({ children }) {
   return (
     <body>
-        <div><SideBar/></div>
+        <div><NavBar/></div>
         
         {children}
     </body>
