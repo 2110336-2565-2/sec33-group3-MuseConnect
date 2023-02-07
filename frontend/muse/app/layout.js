75f11eddcfb2af76f835d1d018ecc8e85538f00a
ohm <ohmkunn@hotmail.com>
2023-02-07 14:50:03 +0700

    responsive and link home

--- frontend/muse/app/layout.js
+++ frontend/muse/app/layout.js
@@ -5,6 +5,9 @@ import Link from 'next/link';
 
 export default function RootLayout({ children }) {
   return (
-    <body>{children}</body>
+    <html lang='en'>
+      <body>{children}</body>
+    </html>
+    
   )
 }
