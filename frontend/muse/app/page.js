5465a432c408b3b7a7b38f15de5fc69f25ec6414
rata <rataprim30026@gmail.com>
2023-02-15 15:53:28 +0700

    add get in touch section

--- frontend/muse/app/page.js
+++ frontend/muse/app/page.js
@@ -6,7 +6,6 @@ import NavBar from '../ui/NavBar'
 import '../app/globals.css'
 import Elehome from '../ui/elehome';
 import React, { useEffect, useState } from "react";
-
 import { Montserrat } from '@next/font/google'
 const montserratBold = Montserrat({ 
   weight: '700',
@@ -22,9 +21,19 @@ export default function Home() {
     setActive(!isActive);
   };
 
+    const [isHovering, setIsHovering] = useState(false);
+  
+    const handleMouseOver = () => {
+      setIsHovering(true);
+    };
+  
+    const handleMouseOut = () => {
+      setIsHovering(false);
+    };
+
   return(
     <>
-        <div className="wrapper d-flex align-items-stretch">
+      <div className="wrapper d-flex align-items-stretch">
 			<SideBar children={isActive}/>
 
         {/* <!-- Page Content  --> */}
@@ -32,9 +41,20 @@ export default function Home() {
         <NavBar show={handleClick} />
         <div className="p-4 p-md-5">
           <h3 style={{fontSize:"60px", marginBottom: "0px"}} className={montserratBold.className}>Welcome to Muse Connect</h3>
-          <h4 style={{fontSize:"40px", marginBottom:"30px", color:"#26A74C"}} className={montserrat.className}>Music connects us.</h4>
+          <h4 style={{fontSize:"40px", marginBottom:"20px", color:"#26A74C"}} className={montserrat.className}>Music connects us.</h4>
           {/* textDecorationLine:"underline", textDecorationColor:"#26A74C"} */}
           <Elehome/>
+          <h4 onMouseOver={handleMouseOver}
+          onMouseOut={handleMouseOut}
+          style={{fontSize:"30px", marginTop:"15px",textDecoration:"underline", width:"fit-content"}}
+          className={montserrat.className}
+          >
+          Get in {isHovering? "Touchy" : "touch"}</h4>
+          {isHovering && (
+            <h4 
+            style={{fontSize:"15px"}}
+            className={montserrat.className}>Muse Connect - Chula Engineering | ♫⋆｡♪ ₊˚♬ ﾟWe are based in Bangkok, Thailand</h4>
+          )}
         </div>
       </div>
 		</div>
