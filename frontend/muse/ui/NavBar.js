24e5c55d0ff9a42a19b508f7d289c983901ebd7c
G10N(nick) <nick.supakorn@hotmail.com>
2023-03-12 17:14:27 +0700

    fix naxbar

--- frontend/muse/ui/NavBar.js
+++ frontend/muse/ui/NavBar.js
@@ -1,54 +1,58 @@
-'use client'
+"use client";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Container from 'react-bootstrap/Container';
-import Navbar from 'react-bootstrap/Navbar';
-import { Button, Stack } from 'react-bootstrap'
-import React from 'react'
-import Link from 'next/link';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
+import { Button, Stack } from "react-bootstrap";
+import React from "react";
+import Link from "next/link";
 // import styles from './Navbar.css'
-import { Montserrat } from '@next/font/google'
-import { FaBars } from 'react-icons/fa';
-const montserrat = Montserrat({ subsets: ['latin'] })
+import { Montserrat } from "@next/font/google";
+import { FaBars } from "react-icons/fa";
+const montserrat = Montserrat({ subsets: ["latin"] });
 
-export default function NavBar({show}) {
-
-  const user = localStorage.getItem('user');
+export default function NavBar({ show }) {
+  const user = localStorage.getItem("user");
   const logOut = () => {
-    localStorage.removeItem('user');
-  }; 
+    localStorage.removeItem("user");
+  };
 
-  if(user == null){
+  if (user == null) {
     return (
-      <Navbar style={{backgroundColor:"rgba(16, 16, 16, 1)"}} expand="lg">
+      <Navbar style={{ backgroundColor: "rgba(16, 16, 16, 1)" }} expand="lg">
         <Container>
-          <Button id = "menuButton" onClick={show} >
-            <FaBars/>
+          <Button id="menuButton" onClick={show}>
+            <FaBars />
           </Button>
-          <Stack direction='horizontal' gap={3}>
+          <Stack direction="horizontal" gap={3}>
             <button className="btn btn-outline-dark">
-              <a href="/Signup" className={montserrat.className}>Sign up</a>
+              <a href="/Signup" className={montserrat.className}>
+                Sign up
+              </a>
             </button>
             <button className="btn btn-success ">
-              <a href="/Login" className={montserrat.className}>Log in</a>
+              <a href="/Login" className={montserrat.className}>
+                Log in
+              </a>
             </button>
           </Stack>
         </Container>
       </Navbar>
-    )
+    );
   } else {
-    return(
-    <Navbar style={{backgroundColor:"rgba(16, 16, 16, 1)"}} expand="lg">
+    return (
+      <Navbar style={{ backgroundColor: "rgba(16, 16, 16, 1)" }} expand="lg">
         <Container>
-          <Button id = "menuButton" onClick={show} >
-          <FaBars/>
+          <Button id="menuButton" onClick={show}>
+            <FaBars />
           </Button>
-            <button className="btn btn-success" onClick={logOut}>
-              <a href="" className={montserrat.className}>Log out</a>
-            </button>
+          <button className="btn btn-success" onClick={logOut}>
+            <a href="/" className={montserrat.className}>
+              Log out
+            </a>
+          </button>
         </Container>
       </Navbar>
-    )
+    );
   }
-  
 }
