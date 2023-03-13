17e3c82fcbbdc4c39249615dd82388023105a177
ohm <ohmkunn@hotmail.com>
2023-03-13 15:44:45 +0700

    new chatroom html

--- frontend/muse/app/Chat/page.js
+++ frontend/muse/app/Chat/page.js
@@ -1,27 +1,25 @@
 'use client'
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-import SideBar from '../../ui/SideBar'
+import Chatsidebar from '../../ui/Chat2/Chatsidebar'
 import NavBar from '../../ui/NavBar'
 import '../../style/style2.css'
 import React, { useState } from "react";
 
-export default function RootLayout({ children }) {
+export default function RootLayout() {
   const [isActive, setActive] = useState("false");
   const handleClick = () => {
-    console.log('hello world');
     setActive(!isActive);
   };
   return (
     <>
       <div className="wrapper d-flex align-items-stretch">
-        <SideBar children={isActive}/>
+        <Chatsidebar children={isActive}/>
 
     {/* <!-- Page Content  --> */}
         <div id="content">
           <NavBar show={handleClick} />
           <div className="p-4 p-md-5">
-            {children}
           </div>
         </div>
       </div>
