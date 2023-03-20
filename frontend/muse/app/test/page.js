880ada16cc2ccad1a24b96eb8c9a98916d136f28
G10N(nick) <nick.supakorn@hotmail.com>
2023-03-20 20:43:46 +0700

    access chat in test page

--- frontend/muse/app/test/page.js
+++ frontend/muse/app/test/page.js
@@ -2,27 +2,45 @@
 import { useState } from "react";
 import { Button, Modal } from "react-bootstrap";
 
-export default function page() {
-  const [showModal, setShowModal] = useState(false);
-  const handleCloseModal = () => setShowModal(false);
-  const handleShowModal = () => setShowModal(true);
-  return (
-    <div>
-      <Button variant="primary" onClick={handleShowModal}>
-        Click me to say hello!
-      </Button>
+const chatHandler = async (userId) => {
+  const user = await JSON.parse(localStorage.getItem("user")); // {email : ,token : ,_id : }
+  if (user) {
+    const respone = await fetch("http://localhost:4000/api/chat", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        authorization: `Bearer ${user.token}`,
+      },
+      body: JSON.stringify({ userId }),
+    });
 
-      <Modal show={showModal} onHide={handleCloseModal}>
-        <Modal.Header closeButton>
-          <Modal.Title>Hello</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>Hello, world!</Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleCloseModal}>
-            Close
-          </Button>
-        </Modal.Footer>
-      </Modal>
-    </div>
+    const result = await respone.json();
+
+    if (respone.ok) {
+      window.location.href = `/Chat/${result._id}`;
+    } else {
+      alert(result.error);
+    }
+  } else {
+    alert("please login");
+  }
+};
+
+export default function page() {
+  const musicianId = "63f8ddb2f1b76a92b35cb13d";
+  const organizerId = "63f8ddfdf1b76a92b35cb143";
+  return (
+    <>
+      <Button onClick={() => chatHandler(organizerId)}>
+        Chat with random people
+      </Button>
+      <Button
+        onClick={() => {
+          window.location.href = "/Chat";
+        }}
+      >
+        check redirect
+      </Button>
+    </>
   );
 }
