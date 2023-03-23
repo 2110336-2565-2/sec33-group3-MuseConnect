89f3ba6cfddb2132a74bbb3f410e2637f420ea6b
G10N(nick) <nick.supakorn@hotmail.com>
2023-03-23 21:19:43 +0700

    update profile chat

--- frontend/muse/app/test/page.js
+++ frontend/muse/app/test/page.js
@@ -1,34 +1,39 @@
 "use client";
-import { useState } from "react";
-import { Button, Modal } from "react-bootstrap";
-
-const chatHandler = async (userId) => {
-  const user = await JSON.parse(localStorage.getItem("user")); // {email : ,token : ,_id : }
-  if (user) {
-    const respone = await fetch("http://localhost:4000/api/chat", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        authorization: `Bearer ${user.token}`,
-      },
-      body: JSON.stringify({ userId }),
-    });
-
-    const result = await respone.json();
-
-    if (respone.ok) {
-      window.location.href = `/Chat/${result._id}`;
-    } else {
-      alert(result.error);
-    }
-  } else {
-    alert("please login");
-  }
-};
+import { Button} from "react-bootstrap";
+import { useRouter } from "next/navigation";
 
 export default function page() {
+
   const musicianId = "63f8ddb2f1b76a92b35cb13d";
   const organizerId = "63f8ddfdf1b76a92b35cb143";
+  const router = useRouter();
+
+  const chatHandler = async (userId) => {
+    const user = await JSON.parse(localStorage.getItem("user")); // {email : ,token : ,_id : }
+    if (user) {
+      const respone = await fetch("http://localhost:4000/api/chat", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          authorization: `Bearer ${user.token}`,
+        },
+        body: JSON.stringify({ userId }),
+      });
+  
+      const result = await respone.json();
+  
+      if (respone.ok) {
+        router.push(`/Chat/${result._id}`);
+        // router.push(`/`)
+        // window.location.href = `/Chat/${result._id}`;
+      } else {
+        alert(result.error);
+      }
+    } else {
+      alert("please login");
+    }
+  };
+
   return (
     <>
       <Button onClick={() => chatHandler(organizerId)}>
