aaf2300b2f6fc2bad161c907924140b91e580133
ThanachatAim <Thanachat.aim@gmail.com>
2023-03-26 06:56:13 +0700

    api doc for porfolio

--- server/routes/portfolio.js
+++ server/routes/portfolio.js
@@ -10,6 +10,104 @@ const {
 } = require('../controllers/portfolioController')
 const requireAuth = require('../middleware/requireAuth')
 
+/**
+* @swagger
+* tags:
+*   name: Portfolios
+*   description: Portfolios managing API
+*/
+/**
+* @swagger
+* /portfolio:
+*   get:
+*       summary: get all portfolios
+*       tags: [Portfolios]
+*       responses:
+*           200:
+*               description: The list of the portfolios
+*           400:
+*               description: Some error
+*/
+/**
+* @swagger
+* /portfolio/{id}:
+*   get:
+*     summary: Get the portfolio by id
+*     tags: [Portfolios]
+*     parameters:
+*       - in: path
+*         name: id
+*         schema:
+*           type: string
+*         required: true
+*         description: The portfolio id
+*     responses:
+*       200:
+*         description: the portfolio description by id
+*       400:
+*         description: Some error
+*       404:
+*         description: The portfolio was not found
+*/
+/**
+* @swagger
+* /portfolio:
+*   post:
+*     summary: create portfolio
+*     tags: [Portfolios]
+*     requestBody:
+*       required: true
+*     responses:
+*       201:
+*         description: Create successfull
+*       400:
+*         description: Some error
+*/
+/**
+* @swagger
+* /portfolio/{id}:
+*   put:
+*     summary: Update the portfolio by the id
+*     tags: [Portfolios]
+*     parameters:
+*       - in: path
+*         name: id
+*         schema:
+*           type: string
+*         required: true
+*         description: The portfolio id
+*     requestBody:
+*       required: true
+*     responses:
+*       200:
+*         description: The portfolio was updated
+*       400:
+*         description: Some error
+*       404:
+*         description: The portfolio was not found 
+*/
+/**
+* @swagger
+* /portfolio/{id}:
+*   delete:
+*     summary: Remove the portfolio by id
+*     tags: [Portfolios]
+*     parameters:
+*       - in: path
+*         name: id
+*         schema:
+*           type: string
+*         required: true
+*         description: The portfolio id
+*     responses:
+*       200:
+*         description: The portfolio was deleted
+*       400:
+*         description: Some error
+*       404:
+*         description: The portfolio was not found
+*/
+
 router.use(requireAuth)
 
 // get portfolios
